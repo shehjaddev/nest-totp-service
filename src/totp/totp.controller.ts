e650650071b8f30914873af371d9805b48c1e44a
shehjaddev <shehjadcse@gmail.com>
2025-08-12 23:06:58 +0600

    feat: add terminal QR code display with qrcode-terminal package

--- src/totp/totp.controller.ts
+++ src/totp/totp.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { TotpService } from './totp.service';
 import * as QRCode from 'qrcode';
+import * as qrcodeTerminal from 'qrcode-terminal';
+import { TotpService } from './totp.service';
 
 @Controller('totp')
 export class TotpController {
@@ -16,6 +17,11 @@ export class TotpController {
         const { secret, otpauth } = this.totpService.generateSecret(email);
 
         const qrCodeDataURL = await QRCode.toDataURL(otpauth);
+
+        qrcodeTerminal.generate(otpauth, { small: true }, (qrcode) => {
+            console.log('\nScan this QR code with your Authenticator app:\n');
+            console.log(qrcode);
+        });
 
         return { secret, otpauth, qrCodeDataURL };
     }
