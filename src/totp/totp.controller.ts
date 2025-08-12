bf0500c205bd45f0d6cf6b218d379302b30be474
shehjaddev <shehjadcse@gmail.com>
2025-08-12 23:03:00 +0600

    feat: add TOTP secret generation and provisioning endpoint with QR code

--- src/totp/totp.controller.ts
+++ src/totp/totp.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TotpService } from './totp.service';
+import * as QRCode from 'qrcode';
 
 @Controller('totp')
 export class TotpController {
+    constructor(private readonly totpService: TotpService) { }
+
     @Get("health")
     healthCheck() {
         return { status: "ok" };
+    }
+
+    @Post('setup')
+    async setup(@Body('email') email: string) {
+        const { secret, otpauth } = this.totpService.generateSecret(email);
+
+        const qrCodeDataURL = await QRCode.toDataURL(otpauth);
+
+        return { secret, otpauth, qrCodeDataURL };
     }
 }
--- src/totp/totp.service.ts
+++ src/totp/totp.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { authenticator } from 'otplib';
 
 @Injectable()
-export class TotpService {}
+export class TotpService {
+    generateSecret(email: string) {
+        const secret = authenticator.generateSecret();
+
+        const otpauth = authenticator.keyuri(email, 'nest-totp-service', secret);
+
+        return { secret, otpauth };
+    }
+}
